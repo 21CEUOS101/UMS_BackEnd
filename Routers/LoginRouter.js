088d07007bde9941df352346991cb02334f26c09
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-09-19 18:38:10 +0530

    nice

--- Routers/LoginRouter.js
+++ Routers/LoginRouter.js
@@ -6,10 +6,8 @@ const FacultyDetails = require('../Models/FacultyDetails');
 const TTODetails = require("../Models/TTODetails");
 const TPODetails = require("../Models/TPODetails");
 const AdminDetails = require("../Models/AdminDetails");
-const LocalStorage = require('node-localstorage').LocalStorage;
 const LoginModel = require('../Models/Login_Auth/LoginModel');
-
-global.localStorage = new LocalStorage('./scratch');
+const HODDetails = require('../Models/HODDetails');
 
 LoginRouter.post('/', async (req, res) => {
     const { user_id, role, password } = req.body;
@@ -23,44 +21,46 @@ LoginRouter.post('/', async (req, res) => {
             const isMatch = await bcrypt.compare(password, login.password);
             console.log(isMatch);
 
-            if (isMatch) {
-
-                req.session.role = role;
-                req.session.user_id = user_id;
-                localStorage.setItem('user_id', user_id);
-                localStorage.setItem('role', role);
-                localStorage.setItem('secret-key', req.body.key);
-
-                if (role === 'student') {
-                    const Student = await StudentContactInfo.find({ student_id: user_id });
-                    req.session.email = Student.email;
-                    localStorage.setItem('email', Student.email);
-                }
-                else if (role === 'faculty') {
-                    const Faculty = await FacultyDetails.find({ faculty_id: user_id });
-                    req.session.email = Faculty.faculty_email;
-                    localStorage.setItem('email', Faculty.faculty_email);
-                }
-                else if (role === 'tto') {
-                    const TTO = await TTODetails.find({ tto_id: user_id });
-                    req.session.email = TTO.tto_email;
-                    localStorage.setItem('email', TTO.tto_email);
-                }
-                else if (role === 'tpo') {
-                    const TPO = await TPODetails.find({ tpo_id: user_id });
-                    req.session.email = TPO.tpo_email;
-                    localStorage.setItem('email', TPO.tpo_email);
-                }
-                else if (role === 'admin') {
-                    const Admin = await AdminDetails.find({ admin_id: user_id });
-                    req.session.email = Admin.admin_email;
-                    localStorage.setItem('email', Admin.admin_email);
-                }
-                else if (role === 'hod') {
-                    const HOD = await FacultyDetails.find({ faculty_id: user_id });
-                    req.session.email = HOD.faculty_email;
-                    localStorage.setItem('email', HOD.faculty_email);
-                }
+            if (isMatch === true) {
+                // req.session.role = role;
+                // req.session.user_id = user_id;
+                // localStorage.setItem('user_id', user_id);
+                // console.log(localStorage.getItem('user_id'));
+                // localStorage.setItem('role', role);
+                // localStorage.setItem('secret-key', req.body?.key);
+                // console.log(role==='hod');
+                
+                // if (role === 'student') {
+                //     const Student = await StudentContactInfo.find({ student_id: user_id });
+                //     req.session.email = Student.email;
+                //     localStorage.setItem('email', Student.email);
+                // }
+                // else if (role === 'faculty') {
+                //     const Faculty = await FacultyDetails.find({ faculty_id: user_id });
+                //     req.session.email = Faculty.faculty_email;
+                //     localStorage.setItem('email', Faculty.faculty_email);
+                // }
+                // else if (role === 'tto') {
+                //     const TTO = await TTODetails.find({ tto_id: user_id });
+                //     req.session.email = TTO.tto_email;
+                //     localStorage.setItem('email', TTO.tto_email);
+                // }
+                // else if (role === 'tpo') {
+                //     const TPO = await TPODetails.find({ tpo_id: user_id });
+                //     req.session.email = TPO.tpo_email;
+                //     localStorage.setItem('email', TPO.tpo_email);
+                // }
+                // else if (role === 'admin') {
+                //     const Admin = await AdminDetails.find({ admin_id: user_id });
+                //     req.session.email = Admin.admin_email;
+                //     localStorage.setItem('email', Admin.admin_email);
+                // }
+                // else if (role === 'hod') {
+                //     console.log("HOD");
+                //     const HOD = await HODDetails.find({ hod_id: user_id });
+                //     req.session.email = HOD.hod_email;
+                //     // localStorage.setItem('email', HOD.hod_email);
+                // }
 
                 console.log("Successfully logged in");
                 // Login successful
