b160b9e75ca21278ab5b00b0e161857e5ff04ee6
manthan161203 <manthanlp3204@gmail.com>
2023-09-24 11:53:08 +0530

    mail

--- Functionalities/MakeAnnouncement.js
+++ Functionalities/MakeAnnouncement.js
@@ -3,57 +3,79 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const LocalStorage = require('node-localstorage').LocalStorage;
 const localStorage = new LocalStorage('./scratch');
+const bodyParser = require('body-parser'); // Added bodyParser for JSON parsing
+
 // Configure Nodemailer with Gmail credentials
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: localStorage.getItem('email'),
-    pass: '', // Initially empty, will be updated later
-  },
-});
 
-router.use(express.json());
+router.use(bodyParser.json()); // Use bodyParser for JSON parsing
 
-// Enable Nodemailer logging
-transporter.on('log', (data) => {
-  console.log(data);
-});
+
 
 // API endpoint to handle sending emails
-router.post('/make-announcement', (req, res) => {
-  // Assuming req.body.students is an array of student email addresses
-  const students = req.body.students;
-  const bcc = req.body.bcc;
-  const secretKey = req.body.secretKey; // Get the secretKey from req.body
+router.post('/make-announcement', async (req, res) => {
 
-  // Update the transporter's pass with the secretKey from req.body
-  transporter.options.auth.pass = secretKey;
+  try {
 
-  // Make sure 'students' is an array before calling 'join'
-  if (Array.isArray(students) && Array.isArray(bcc)) {
+    // Configure Nodemailer with Gmail credentials
+    let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: req.body.from,
+        pass: req.body.secretKey, // Initially empty, will be updated later
+      },
+    });
+
+    // Enable Nodemailer logging
+    transporter.on('log', (data) => {
+      console.log(data);
+    });
+
+    // Assuming req.body.students is an array of student email addresses
+    const students = req.body.students;
+    const bcc = req.body.bcc;
+    // const secretKey = req.body.secretKey; // Get the secretKey from req.body
+    // console.log(secretKey);
+    // transporter.options.auth.pass = secretKey;
+    console.log(transporter.options.auth);
+
+    // Check if 'students' and 'bcc' are arrays
+    if (!Array.isArray(students) || !Array.isArray(bcc)) {
+      return res.status(400).json({ error: 'Invalid input: students and bcc should be arrays' });
+    }
+
     const subject = req.body.subject;
     const text = req.body.text;
 
+    // Check if required fields are provided
+    if (!subject || !text) {
+      return res.status(400).json({ error: 'Missing required fields: subject, text, or secretKey' });
+    }
+
+    // Update the transporter's pass with the secretKey from req.body
+    
+
     // Create the email content
     const mailOptions = {
-      from: localStorage.getItem('email'), // Use the email from localStorage
+      from: "dduums123@gmail.com",
       to: students.join(', '),
       bcc: bcc.join(', '),
       subject: subject,
       text: text,
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending email:', error);
-        res.status(500).json({ message: 'Error sending email' });
-      } else {
-        console.log('Email sent:', info.response);
-        res.status(200).json({ message: 'Email sent successfully' });
-      }
-    });
-  } else {
-    res.status(400).json({ error: 'Invalid input: students should be an array' });
+    // Use async/await to handle sending email
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log('Email sent:', info.response);
+    return res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+
+    if (error.code === 'EAUTH') {
+      return res.status(401).json({ error: 'Authentication failed. Invalid secretKey.' });
+    } else {
+      return res.status(500).json({ error: 'Error sending email' });
+    }
   }
 });
 
