430bb256a546712d13e447f8a0efb462d3e61839
21CEUOS101 <21ceuos101@ddu.ac.in>
2023-11-13 08:39:18 +0530

    env setted

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const PORT = 3001;
 const mongoose = require("mongoose");
-const url = "mongodb+srv://manthan1:manthan1@project.vpqiqxs.mongodb.net/UMS";
+const dotenv = require("dotenv").config({path : './.env'});
+const url = process.env.MONGODB_CONNECT_URL;
 
 mongoose.connect(url, { useNewUrlParser: true });
 
